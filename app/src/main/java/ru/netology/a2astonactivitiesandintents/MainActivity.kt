b2b72881807e5994ca8725550c1740958d4e1216
SumbulIshdavletova <ishdavletova.sumbul@gmail.com>
2023-03-25 18:48:05 +0500

    Initial Commit

--- app/src/main/java/ru/netology/a2astonactivitiesandintents/MainActivity.kt
+++ app/src/main/java/ru/netology/a2astonactivitiesandintents/MainActivity.kt
@@ -1,42 +1,94 @@
 package ru.netology.a2astonactivitiesandintents
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
-import android.view.View
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ShareCompat
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val LOG_TAG = MainActivity::class.java.simpleName
-    private var mCount = 0
-    private var mShowCount: TextView? = null
-    val EXTRA_MESSAGE = "ru.netology.a2astonactivitiesandintents.MESSAGE"
+    private var mWebsiteEditText: EditText? = null
+    private var mLocationEditText: EditText? = null
+    private var mShareTextEditText: EditText? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mShowCount = findViewById(R.id.show_count)
+        mWebsiteEditText = findViewById(R.id.website_edittext)
+        mLocationEditText = findViewById(R.id.location_edittext)
+        mShareTextEditText = findViewById(R.id.share_edittext)
+
+        val openWebsite = findViewById<Button>(R.id.open_website_button)
+        openWebsite.setOnClickListener {
+            openWebsite()
+        }
+        val openLoc = findViewById<Button>(R.id.open_location_button)
+        openLoc.setOnClickListener {
+            openLocation()
+        }
+
+        val shareText = findViewById<Button>(R.id.share_text_button)
+        shareText.setOnClickListener {
+            shareText()
+        }
+        val camera = findViewById<Button>(R.id.take_picture_button)
+        camera.setOnClickListener {
+            takePictureIntent()
+        }
 
     }
 
-    fun launchSecondActivity(view: View) {
-        val intent = Intent(this, SecondActivity::class.java)
-
-        val message = mCount.toString()
-        intent.putExtra("EXTRA_MESSAGE", message)
-        startActivity(intent)
-
-    }
-
-    fun countUp(view: View?) {
-        mCount++
-        if (mShowCount != null) {
-            mShowCount!!.text = mCount.toString()
+    private fun openWebsite() {
+        val url = mWebsiteEditText?.text.toString()
+        val webpage: Uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Log.d("ImplicitIntents", "Open Website Error")
         }
     }
+
+    private fun openLocation() {
+        val loc = mLocationEditText?.text.toString()
+
+        val addressUri = Uri.parse("geo:0,0?q=$loc")
+        val intent = Intent(Intent.ACTION_VIEW, addressUri)
+        try {
+            startActivity(intent)
+        } catch (ex: ActivityNotFoundException) {
+            Log.d("ImplicitIntents", "Open Location Error")
+        }
+    }
+
+    private fun shareText() {
+        val txt = mShareTextEditText?.text.toString()
+        val mimeType = "text/plain"
+        ShareCompat.IntentBuilder(this)
+            .setType(mimeType)
+            .setChooserTitle(R.string.share_text_with)
+            .setText(txt)
+            .startChooser()
+    }
+
+    private fun takePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivity(takePictureIntent)
+        } catch (e: ActivityNotFoundException) {
+            Log.d("ImplicitIntents", "Camera error")
+        }
+    }
+
 }
+
+//постараться убрать все !! во всех ветках
